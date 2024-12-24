b3d8e2a98a348c6297dc8a97ef3eb55c3b7c7610
Nguyễn Hoàng HIệp <67302956+hiep1512001@users.noreply.github.com>
2024-12-24 08:21:29 +0700

    create app

---
+++ appbvnd115/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.appbvnd115',
+  appName: 'appbvnd115',
+  webDir: 'dist'
+};
+
+export default config;
